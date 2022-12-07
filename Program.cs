14c7dae1096920bd5274f0872b5aa231ad058999
Gabriel Gomes <ggfernandes30@gmail.com>
2022-12-06 22:12:08 -0300

    Calculo de a0 até a6 completo

--- Program.cs
+++ Program.cs
@@ -15,14 +15,65 @@
         float [,] ar_mat = m.ArrayOfArray(matriz);
         float[,] ar_trans = m.ArrayOfArray(ArrayT);
         float[,] ar_vetor = m.ArrayOfArray(vCentro);
+        float norma0 = m.norma(ar_vetor);
+        float[,] a0 = m.mul(1/norma0, ar_vetor);
+        //m.PrintPrettyVetor(a0);
 
         float[,] resMulArr = m.dot(ar_mat, ar_trans);
-        //m.PrintPrettyArray(resMulArr);
-        float[,] resMulArrVetor = m.dot(resMulArr, ar_vetor);
-        //m.PrintPrettyArray(resMulArrVetor);
-        float norma = m.norma(resMulArrVetor);
-        //Console.WriteLine(norma);
-        float[,] a1 = m.mul(1/norma, resMulArrVetor);
+        //A1 passo a passo formula
+        #region A1
+        float[,] resMulArrVetor1 = m.dot(resMulArr, a0);
+        float norma1 = m.norma(resMulArrVetor1);
+        float[,] a1 = m.mul(1/norma1, resMulArrVetor1);
+        Console.WriteLine("A1: ");
         m.PrintPrettyVetor(a1);//Printa só o vetor tirando os 0's
+        Console.WriteLine("\n");
+        #endregion
+
+        #region A2
+        float[,] resMulArrVetor2 = m.dot(resMulArr, a1);
+        float norma2 = m.norma(resMulArrVetor2);
+        float[,] a2 = m.mul(1/norma2, resMulArrVetor2);
+        Console.WriteLine("A2: ");
+        m.PrintPrettyVetor(a2);
+        Console.WriteLine("\n");
+        #endregion
+
+        #region A3
+        float[,] resMulArrVetor3 = m.dot(resMulArr, a2);
+        float norma3 = m.norma(resMulArrVetor3);
+        float[,] a3 = m.mul(1/norma3, resMulArrVetor3);
+        Console.WriteLine("A3: ");
+        m.PrintPrettyVetor(a3);
+        Console.WriteLine("\n");
+        #endregion
+
+        #region A4
+        float[,] resMulArrVetor4 = m.dot(resMulArr, a3);
+        float norma4 = m.norma(resMulArrVetor4);
+        float[,] a4 = m.mul(1/norma4, resMulArrVetor4);
+        Console.WriteLine("A4: ");
+        m.PrintPrettyVetor(a4);
+        Console.WriteLine("\n");
+        #endregion
+
+        #region A5
+        float[,] resMulArrVetor5 = m.dot(resMulArr, a4);
+        float norma5 = m.norma(resMulArrVetor5);
+        float[,] a5 = m.mul(1/norma5, resMulArrVetor5);
+        Console.WriteLine("A5: ");
+        m.PrintPrettyVetor(a5);
+        Console.WriteLine("\n");
+        #endregion
+
+        #region A6
+        float[,] resMulArrVetor6 = m.dot(resMulArr, a5);
+        float norma6 = m.norma(resMulArrVetor6);
+        float[,] a6 = m.mul(1/norma6, resMulArrVetor6);
+        Console.WriteLine("A6: ");
+        m.PrintPrettyVetor(a6);
+        Console.WriteLine("\n");
+        #endregion
+
     }
 }
